5af685f79c778c44a79b32ec1799a6a994a6d7d6
Chris D'Ambrosio <cdambrosio@gmail.com>
2021-08-04 21:51:43 -0700

    Remove boilerplate

--- lib/jeff.ex
+++ lib/jeff.ex
@@ -1,18 +1,2 @@
 defmodule Jeff do
-  @moduledoc """
-  Documentation for `Jeff`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Jeff.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/jeff_test.exs
+++ test/jeff_test.exs
@@ -1,8 +1,4 @@
 defmodule JeffTest do
   use ExUnit.Case
   doctest Jeff
-
-  test "greets the world" do
-    assert Jeff.hello() == :world
-  end
 end
