2edf8636e380794d025c2db33875bd2c535738d9
Chris D'Ambrosio <cdambrosio@gmail.com>
2022-02-10 09:39:05 -0800

    v0.2.0

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Jeff.MixProject do
   use Mix.Project
 
+  @version "0.2.0"
+
   def project do
     [
       app: :jeff,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
